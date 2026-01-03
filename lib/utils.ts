dad02cf8163fc33a689166ced1cf50fcca1055b6
Guillemuhana <guillemuhana@gmail.com>
2026-01-03 15:14:54 -0300

    Fix lib/utils.ts export

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,30 +1,6 @@
-﻿export interface User {
-  id: number
-  username: string
-  password: string
-  familia: string
-  direccion: string
-  email: string
-  localidad: string
-  codigo_postal: string
-  foto_fachada: string | null
-  qr_code: string
-  created_at: string
-}
+﻿import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
-export interface LoginCredentials {
-  username: string
-  password: string
-}
-
-export interface AuthToken {
-  userId: number
-  username: string
-  familia: string
-}
-
-export interface AuthResponse {
-  success: boolean
-  user: Omit<User, "password">
-  token: string
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
